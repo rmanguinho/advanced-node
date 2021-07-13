1b4cceb7e3f93bcb4f3a3d4f97ebe9ab92339ff4
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-13 17:25:27 -0300

    ensure JwtTokenGenerator rethows if sign throws

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -37,4 +37,12 @@ describe('JwtTokenGenerator', () => {
 
     expect(token).toBe('any_token')
   })
+
+  it('should rethrow if sign throws', async () => {
+    fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
+
+    const promise = sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    await expect(promise).rejects.toThrow(new Error('token_error'))
+  })
 })
