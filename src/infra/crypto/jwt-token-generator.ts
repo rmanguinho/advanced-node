a0ffad4ff0cc4ac4adf3f607864b8572135bd23b
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-13 17:27:11 -0300

    move JwtTokenGenerator to its owen file

---
+++ src/infra/crypto/jwt-token-generator.ts
@@ -0,0 +1,12 @@
+import { TokenGenerator } from '@/data/contracts/crypto'
+
+import { sign } from 'jsonwebtoken'
+
+export class JwtTokenGenerator {
+  constructor (private readonly secret: string) {}
+
+  async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
+    const expirationInSeconds = params.expirationInMs / 1000
+    return sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
+  }
+}
--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -1,17 +1,8 @@
-import { TokenGenerator } from '@/data/contracts/crypto'
+import { JwtTokenGenerator } from '@/infra/crypto'
 
 import jwt from 'jsonwebtoken'
 
 jest.mock('jsonwebtoken')
-
-class JwtTokenGenerator {
-  constructor (private readonly secret: string) {}
-
-  async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
-    const expirationInSeconds = params.expirationInMs / 1000
-    return jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
-  }
-}
 
 describe('JwtTokenGenerator', () => {
   let sut: JwtTokenGenerator
