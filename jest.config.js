4f04a6469d2636dfda356ac91d673c225d690321
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-13 14:40:06 -0300

    ignore index files and main layer from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {
