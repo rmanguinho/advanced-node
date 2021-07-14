6a15b3ac1e2b9ace69d012ddebf21f258f6ce348
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-14 12:52:46 -0300

    make AxiosHttpClient implement HttpGetClient

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,7 +2,7 @@ import { HttpGetClient } from '@/infra/http'
 
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get <T = any> (args: HttpGetClient.Params): Promise<T> {
     const result = await axios.get(args.url, { params: args.params })
     return result.data
