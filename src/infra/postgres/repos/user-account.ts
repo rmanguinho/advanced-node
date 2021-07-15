ce58e357b6a7b8b1319a02922b84700f68b30dab
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-15 13:20:51 -0300

    ensure PgUserAccountRepository updates account if id is defined

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -17,10 +17,19 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
 
   async saveWithFacebook (params: SaveFacebookAccountRepository.Params): Promise<void> {
     const pgUserRepo = getRepository(PgUser)
-    await pgUserRepo.save({
-      email: params.email,
-      name: params.name,
-      facebookId: params.facebookId
-    })
+    if (params.id === undefined) {
+      await pgUserRepo.save({
+        email: params.email,
+        name: params.name,
+        facebookId: params.facebookId
+      })
+    } else {
+      await pgUserRepo.update({
+        id: parseInt(params.id)
+      }, {
+        name: params.name,
+        facebookId: params.facebookId
+      })
+    }
   }
 }
--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -52,5 +52,28 @@ describe('PgUserAccountRepository', () => {
 
       expect(pgUser?.id).toBe(1)
     })
+
+    it('should update account if id is defined', async () => {
+      await pgUserRepo.save({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_fb_id'
+      })
+
+      await sut.saveWithFacebook({
+        id: '1',
+        email: 'new_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
+      const pgUser = await pgUserRepo.findOne({ id: 1 })
+
+      expect(pgUser).toEqual({
+        id: 1,
+        email: 'any_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
+    })
   })
 })
