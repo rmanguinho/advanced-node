0b82de545a2d9af9359b7fcbaeb4f13d35ccd933
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-19 14:46:51 -0300

    apply ok helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,4 +1,4 @@
-import { badRequest, HttpResponse, serverError, unauthorized } from '@/application/helpers'
+import { badRequest, HttpResponse, serverError, unauthorized, ok } from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
@@ -13,12 +13,9 @@ export class FacebookLoginController {
       }
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: accessToken.value
-          }
-        }
+        return ok({
+          accessToken: accessToken.value
+        })
       } else {
         return unauthorized()
       }
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -5,6 +5,11 @@ export type HttpResponse = {
   data: any
 }
 
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  data
+})
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   data: error
