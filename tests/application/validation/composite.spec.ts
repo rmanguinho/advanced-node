68db2b2f070c61ec054921cd2e9857b7410a9257
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-20 15:05:35 -0300

    ensure ValidationComposite returns error

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -42,11 +42,19 @@ describe('ValidationComposite', () => {
   })
 
   it('should return the first error', () => {
-    validator1.validate.mockReturnValue(new Error('error_1'))
-    validator2.validate.mockReturnValue(new Error('error_2'))
+    validator1.validate.mockReturnValueOnce(new Error('error_1'))
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
 
     const error = sut.validate()
 
     expect(error).toEqual(new Error('error_1'))
+  })
+
+  it('should return the error', () => {
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_2'))
   })
 })
