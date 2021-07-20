240adebbb1051abe443d94b77404755ad4281c14
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-20 18:29:38 -0300

    make RequiredStringValidator implements Validator interface

--- src/application/validation/required-string.ts
+++ src/application/validation/required-string.ts
@@ -1,6 +1,7 @@
 import { RequiredFieldError } from '@/application/errors'
+import { Validator } from '@/application/validation'
 
-export class RequiredStringValidator {
+export class RequiredStringValidator implements Validator {
   constructor (
     private readonly value: string,
     private readonly fieldName: string
