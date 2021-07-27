84471f5172b5565de572c26a1a977ee3a3f55f20
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-27 14:41:03 -0300

    add facebook login integration tests

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -9,10 +9,9 @@ type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
 
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
-  private readonly pgUserRepo = getRepository(PgUser)
-
   async load ({ email }: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepo.findOne({ email })
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -22,13 +21,14 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
   }
 
   async saveWithFacebook ({ id, name, email, facebookId }: SaveParams): Promise<SaveResult> {
+    const pgUserRepo = getRepository(PgUser)
     let resultId: string
     if (id === undefined) {
-      const pgUser = await this.pgUserRepo.save({ email, name, facebookId })
+      const pgUser = await pgUserRepo.save({ email, name, facebookId })
       resultId = pgUser.id.toString()
     } else {
       resultId = id
-      await this.pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
+      await pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
     }
     return { id: resultId }
   }
---
+++ tests/main/routes/login.spec.ts
@@ -0,0 +1,52 @@
+import { PgUser } from '@/infra/postgres/entities'
+import { app } from '@/main/config/app'
+import { UnauthorizedError } from '@/application/errors'
+import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+
+import { IBackup } from 'pg-mem'
+import { getConnection } from 'typeorm'
+import request from 'supertest'
+
+describe('Login Routes', () => {
+  describe('POST /login/facebook', () => {
+    let backup: IBackup
+    const loadUserSpy = jest.fn()
+
+    jest.mock('@/infra/apis/facebook', () => ({
+      FacebookApi: jest.fn().mockReturnValue({ loadUser: loadUserSpy })
+    }))
+
+    beforeAll(async () => {
+      const db = await makeFakeDb([PgUser])
+      backup = db.backup()
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    beforeEach(() => {
+      backup.restore()
+    })
+
+    it('should return 200 with AccessToken', async () => {
+      loadUserSpy.mockResolvedValueOnce({ facebookId: 'any_id', name: 'any_name', email: 'any_email' })
+
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'valid_token' })
+
+      expect(status).toBe(200)
+      expect(body.accessToken).toBeDefined()
+    })
+
+    it('should return 401 with UnauthorizedError', async () => {
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'invalid_token' })
+
+      expect(status).toBe(401)
+      expect(body.error).toBe(new UnauthorizedError().message)
+    })
+  })
+})
