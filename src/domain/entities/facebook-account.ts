dabd7fabf0a3165a3a60a52d4909dffb51603a4b
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-07-29 13:02:44 -0300

    improve code quality

--- src/domain/entities/facebook-account.ts
+++ src/domain/entities/facebook-account.ts
@@ -1,13 +1,5 @@
-type FacebookData = {
-  name: string
-  email: string
-  facebookId: string
-}
-
-type AccountData = {
-  id?: string
-  name?: string
-}
+type FacebookData = { name: string, email: string, facebookId: string }
+type AccountData = { id?: string, name?: string }
 
 export class FacebookAccount {
   id?: string
