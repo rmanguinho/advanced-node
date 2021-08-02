03afe70be9b7d146be4e91b2444cf334cb7fac48
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-02 12:29:48 -0300

    improve code quality

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -10,14 +10,18 @@ class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    if (error !== undefined) return forbidden()
+    if (!this.validate({ authorization })) return forbidden()
     try {
       const userId = await this.authorize({ token: authorization })
       return ok({ userId })
     } catch {
       return forbidden()
     }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
   }
 }
 
