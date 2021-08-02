fe4e698c118aa7fe3c4be5bf2ac466b86ed7e97a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-02 10:13:27 -0300

    ensure JwtTokenHandler throws on incorrect cases

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -74,5 +74,21 @@ describe('JwtTokenHandler', () => {
 
       expect(generatedKey).toBe(key)
     })
+
+    it('should rethrow if verify throws', async () => {
+      fakeJwt.verify.mockImplementationOnce(() => { throw new Error('key_error') })
+
+      const promise = sut.validateToken({ token })
+
+      await expect(promise).rejects.toThrow(new Error('key_error'))
+    })
+
+    it('should throw if verify returns null', async () => {
+      fakeJwt.verify.mockImplementationOnce(() => null)
+
+      const promise = sut.validateToken({ token })
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
