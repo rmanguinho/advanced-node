6eefd2220fea25ae4f46fb5875370a8c70fdfad4
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-04 12:45:32 -0300

    improve code quality

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -7,8 +7,8 @@ type Adapter = (middleware: Middleware) => RequestHandler
 export const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
   const { statusCode, data } = await middleware.handle({ ...req.headers })
   if (statusCode === 200) {
-    const entries = Object.entries(data).filter(entry => entry[1])
-    req.locals = { ...req.locals, ...Object.fromEntries(entries) }
+    const validEntries = Object.entries(data).filter(([, value]) => value)
+    req.locals = { ...req.locals, ...Object.fromEntries(validEntries) }
     next()
   } else {
     res.status(statusCode).json({ error: data.message })
