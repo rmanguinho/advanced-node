e5c82be8b75ce39729e291f3351791c2372cfefd
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-11 16:31:48 -0300

    isolate SUT creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -12,18 +12,23 @@ class UUIDHandler {
 }
 
 describe('UUIDHandler', () => {
-  it('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeAll(() => {
+    mocked(v4).mockReturnValue('any_uuid')
+  })
+
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+
+  it('should call uuid.v4', () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
 
   it('should return correct uuid', () => {
-    mocked(v4).mockReturnValueOnce('any_uuid')
-    const sut = new UUIDHandler()
-
     const uuid = sut.uuid({ key: 'any_key' })
 
     expect(uuid).toBe('any_key_any_uuid')
