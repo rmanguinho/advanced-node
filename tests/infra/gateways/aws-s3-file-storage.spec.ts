fa5b6e405ba6408d9c61cf5fa9a0555f8141273a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-12 17:45:26 -0300

    ensure AwsS3FileStorage rethrows when deleteObject throws

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -101,5 +101,14 @@ describe('AwsS3FileStorage', () => {
       expect(deleteObjectSpy).toHaveBeenCalledTimes(1)
       expect(deleteObjectPromiseSpy).toHaveBeenCalledTimes(1)
     })
+
+    it('should rethrow if deleteObject throws', async () => {
+      const error = new Error('delete_error')
+      deleteObjectPromiseSpy.mockRejectedValueOnce(error)
+
+      const promise = sut.delete({ key })
+
+      await expect(promise).rejects.toThrow(error)
+    })
   })
 })
