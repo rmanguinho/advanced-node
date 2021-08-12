f74185adfcf4b267749e77f113083f1b8ce994f3
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-12 17:44:22 -0300

    ensure AwsS3FileStorage calls deleteObject with correct input

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -1,8 +1,8 @@
-import { UploadFile } from '@/domain/contracts/gateways'
+import { UploadFile, DeleteFile } from '@/domain/contracts/gateways'
 
 import { config, S3 } from 'aws-sdk'
 
-export class AwsS3FileStorage implements UploadFile {
+export class AwsS3FileStorage implements UploadFile, DeleteFile {
   constructor (accessKey: string, secret: string, private readonly bucket: string) {
     config.update({
       credentials: {
@@ -21,5 +21,13 @@ export class AwsS3FileStorage implements UploadFile {
       ACL: 'public-read'
     }).promise()
     return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`
+  }
+
+  async delete ({ key }: DeleteFile.Input): Promise<void> {
+    const s3 = new S3()
+    await s3.deleteObject({
+      Bucket: this.bucket,
+      Key: key
+    }).promise()
   }
 }
--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -80,4 +80,26 @@ describe('AwsS3FileStorage', () => {
       await expect(promise).rejects.toThrow(error)
     })
   })
+
+  describe('delete', () => {
+    let deleteObjectPromiseSpy: jest.Mock
+    let deleteObjectSpy: jest.Mock
+
+    beforeAll(() => {
+      deleteObjectPromiseSpy = jest.fn()
+      deleteObjectSpy = jest.fn().mockImplementation(() => ({ promise: deleteObjectPromiseSpy }))
+      mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({ deleteObject: deleteObjectSpy })))
+    })
+
+    it('should call deleteObject with correct input', async () => {
+      await sut.delete({ key })
+
+      expect(deleteObjectSpy).toHaveBeenCalledWith({
+        Bucket: bucket,
+        Key: key
+      })
+      expect(deleteObjectSpy).toHaveBeenCalledTimes(1)
+      expect(deleteObjectPromiseSpy).toHaveBeenCalledTimes(1)
+    })
+  })
 })
