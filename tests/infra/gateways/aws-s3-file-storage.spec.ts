3466c55bbea58239509bf3815eca4be77b4a1d29
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-12 17:35:00 -0300

    ensure AwsS3FileStorage rethrows when putObject throws

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -86,4 +86,13 @@ describe('AwsS3FileStorage', () => {
 
     expect(imageUrl).toBe(`https://${bucket}.s3.amazonaws.com/any%20key`)
   })
+
+  it('should rethrow if putObject throws', async () => {
+    const error = new Error('upload_error')
+    putObjectPromiseSpy.mockRejectedValueOnce(error)
+
+    const promise = sut.upload({ key, file })
+
+    await expect(promise).rejects.toThrow(error)
+  })
 })
