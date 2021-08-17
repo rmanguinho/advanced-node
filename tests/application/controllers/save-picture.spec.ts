b8268eac85c8a011ba1e8ef72058543cb5f270b2
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-17 17:43:49 -0300

    improve error text

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -14,7 +14,7 @@ class SavePictureController {
 
 class InvalidMymeTypeError extends Error {
   constructor (allowed: string[]) {
-    super(`Unsupported type. Allowed types: ${allowed.join(', ')}`)
+    super(`Unsupported file. Allowed extensions: ${allowed.join(', ')}`)
     this.name = 'InvalidMymeTypeError'
   }
 }
