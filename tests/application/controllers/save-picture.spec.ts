61bac32fada0dfb43c0e92069c34a81e40389c4d
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-17 17:29:19 -0300

    ensure SavePictureController returns 400 if file is empty

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,7 +1,7 @@
 import { RequiredFieldError } from '@/application/errors'
 import { badRequest, HttpResponse } from '@/application/helpers'
 
-type HttpRequest = { file: any }
+type HttpRequest = { file: { buffer: Buffer } }
 type Model = Error
 
 class SavePictureController {
@@ -18,7 +18,7 @@ describe('SavePictureController', () => {
   })
 
   it('should return 400 if file is not provided', async () => {
-    const httpResponse = await sut.handle({ file: undefined })
+    const httpResponse = await sut.handle({ file: undefined as any })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
@@ -27,7 +27,16 @@ describe('SavePictureController', () => {
   })
 
   it('should return 400 if file is not provided', async () => {
-    const httpResponse = await sut.handle({ file: null })
+    const httpResponse = await sut.handle({ file: null as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is empty', async () => {
+    const httpResponse = await sut.handle({ file: { buffer: Buffer.from('') } })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
