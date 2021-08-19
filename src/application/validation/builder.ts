03846873beb38ceb3fbef238331a003549bf3f63
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-08-19 17:26:45 -0300

    improve code quality

--- src/application/validation/builder.ts
+++ src/application/validation/builder.ts
@@ -16,7 +16,7 @@ export class ValidationBuilder {
       this.validators.push(new RequiredBuffer(this.value, this.fieldName))
     } else if (typeof this.value === 'string') {
       this.validators.push(new RequiredString(this.value, this.fieldName))
-    } else if (typeof this.value === 'object') {
+    } else {
       this.validators.push(new Required(this.value, this.fieldName))
       if (this.value.buffer !== undefined) {
         this.validators.push(new RequiredBuffer(this.value.buffer, this.fieldName))
