1944e0010ba27edb6bdc43dc5bd745b289508232
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-09-03 17:13:49 -0300

    ensure DbTransactionController returns same result as decoratee on success

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@/application/controllers'
+import { HttpResponse } from '@/application/helpers'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -8,12 +9,13 @@ class DbTransactionController {
     private readonly db: DbTransaction
   ) {}
 
-  async perform (httpRequest: any): Promise<void> {
+  async perform (httpRequest: any): Promise<HttpResponse | undefined> {
     await this.db.openTransaction()
     try {
-      await this.decoratee.perform(httpRequest)
+      const httpResponse = await this.decoratee.perform(httpRequest)
       await this.db.commit()
       await this.db.closeTransaction()
+      return httpResponse
     } catch {
       await this.db.rollback()
       await this.db.closeTransaction()
@@ -36,6 +38,7 @@ describe('DbTransactionController', () => {
   beforeAll(() => {
     db = mock()
     decoratee = mock()
+    decoratee.perform.mockResolvedValue({ statusCode: 204, data: null })
   })
 
   beforeEach(() => {
@@ -76,5 +79,11 @@ describe('DbTransactionController', () => {
     expect(db.rollback).toHaveBeenCalledTimes(1)
     expect(db.closeTransaction).toHaveBeenCalledWith()
     expect(db.closeTransaction).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return same result as decoratee on success', async () => {
+    const httpResponse = await sut.perform({ any: 'any' })
+
+    expect(httpResponse).toEqual({ statusCode: 204, data: null })
   })
 })
