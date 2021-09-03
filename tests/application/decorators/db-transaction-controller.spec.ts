581b9112fd60365c5bb09524a3ce02cdf5a297e7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-09-03 17:10:41 -0300

    ensure DbTransactionController calls rollback and close transaction on failure

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -10,9 +10,14 @@ class DbTransactionController {
 
   async perform (httpRequest: any): Promise<void> {
     await this.db.openTransaction()
-    await this.decoratee.perform(httpRequest)
-    await this.db.commit()
-    await this.db.closeTransaction()
+    try {
+      await this.decoratee.perform(httpRequest)
+      await this.db.commit()
+      await this.db.closeTransaction()
+    } catch {
+      await this.db.rollback()
+      await this.db.closeTransaction()
+    }
   }
 }
 
@@ -20,6 +25,7 @@ interface DbTransaction {
   openTransaction: () => Promise<void>
   closeTransaction: () => Promise<void>
   commit: () => Promise<void>
+  rollback: () => Promise<void>
 }
 
 describe('DbTransactionController', () => {
@@ -55,6 +61,19 @@ describe('DbTransactionController', () => {
 
     expect(db.commit).toHaveBeenCalledWith()
     expect(db.commit).toHaveBeenCalledTimes(1)
+    expect(db.rollback).not.toHaveBeenCalled()
+    expect(db.closeTransaction).toHaveBeenCalledWith()
+    expect(db.closeTransaction).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call rollback and close transaction on failure', async () => {
+    decoratee.perform.mockRejectedValueOnce(new Error('decoratee_error'))
+
+    await sut.perform({ any: 'any' })
+
+    expect(db.commit).not.toHaveBeenCalled()
+    expect(db.rollback).toHaveBeenCalledWith()
+    expect(db.rollback).toHaveBeenCalledTimes(1)
     expect(db.closeTransaction).toHaveBeenCalledWith()
     expect(db.closeTransaction).toHaveBeenCalledTimes(1)
   })
