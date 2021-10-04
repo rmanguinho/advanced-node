5e09f9d215c3347e6c5e6c4373c8ffd8b38d181c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2021-10-04 11:32:09 -0300

    fix hardcoded value

--- src/application/validation/max-file-size.ts
+++ src/application/validation/max-file-size.ts
@@ -7,7 +7,7 @@ export class MaxFileSize {
   ) {}
 
   validate (): Error | undefined {
-    const maxFileSizeInBytes = 5 * 1024 * 1024
+    const maxFileSizeInBytes = this.maxSizeInMb * 1024 * 1024
     if (this.value.length > maxFileSizeInBytes) return new MaxFileSizeError(this.maxSizeInMb)
   }
 }
